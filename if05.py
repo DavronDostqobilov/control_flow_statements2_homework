bafbc6dc0f4e8fea6a8515c1a37dbb689579a455
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-23 23:19:31 +0300

    if05

--- if05.py
+++ if05.py
@@ -6,4 +6,24 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n=n//10
+    x2=n%10
+    n=n//10
+    x3=n%10
+    n=n//10
+    x4=n%10
+    x5=n//10
+    v=0
+    if v<x1:
+        v=x1
+    if v<x2:
+        v=x2
+    elif v<x3:
+        v=x3
+    elif v<x4:
+        v=x4
+    elif v<x5:
+        v=x5
+    return v
+print(main(56734))
