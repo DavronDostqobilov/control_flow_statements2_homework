e82db672679f886d6b733df915a0dab94953a3ab
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-24 16:35:37 +0300

    if06

--- if06.py
+++ if06.py
@@ -6,4 +6,29 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1=n%10
+    n=n//10
+    x2=n%10
+    n=n//10
+    x3=n%10
+    n=n//10
+    x4=n%10
+    x5=n//10
+    v=0
+    n=5
+    if v<x1:
+        v=x1
+    if v<x2:
+        v=x2
+        n=n-1
+    if v<x3:
+        v=x3
+        n=n-1
+    if v<x4:
+        v=x4
+        n=n-1
+    if v<x5:
+        v=x5
+        n=n-1
+    return n
+print(main(56371))
